8f4a4e47666318ff6714bc19ad5ada0e9a41a957
Rishabh Shah <shahrishabh7@gmail.com>
2023-11-21 18:50:42 -0600

    remove posts router work

---
+++ src/server/api/routers/buildings.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+export const buildingsRouter = createTRPCRouter({
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const buildings = await ctx.db.building.findMany({
+      take: 100,
+      orderBy: [{ createdAt: "desc" }],
+    });
+
+    return buildings;
+  }),
+});
