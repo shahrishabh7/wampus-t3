642b9cc1d64642f042d9fbfec2c038a6cf0dcc38
Rishabh Shah <shahrishabh7@gmail.com>
2023-11-21 18:50:30 -0600

    add schema, connect planetscale, add buildings router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,4 @@
-import { postRouter } from "~/server/api/routers/post";
+import { buildingsRouter } from "~/server/api/routers/buildings";
 import { createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -7,7 +7,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
+  building: buildingsRouter,
 });
 
 // export type definition of API
